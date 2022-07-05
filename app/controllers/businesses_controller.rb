ac149bbb0b7c5eaf67ad216f5bf86d15f25cbab7
Jared Shelby <jared.shelby@yale.edu>
2022-07-05 10:40:56 -0400

    Index, welcome, show page for businesses

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -4,4 +4,14 @@ class BusinessesController < ApplicationController
         @businesses = Business.all
     end
 
+
+    def welcome
+        @business = Business.find(params[:business_id])
+        redirect_to business_path(@business)
+    end
+
+    def show
+        @business = Business.find(params[:id])
+    end
+
 end
