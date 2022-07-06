aba4bc7cf574b2cdb436ef69ddf297e330fa6e21
Bogdan Cuza <bogdancuza@protonmail.com>
2022-07-06 21:27:33 +0300

    Clean up code & fix technician selection

--- app/controllers/technicians_controller.rb
+++
@@ -1,9 +0,0 @@
-class TechniciansController < ApplicationController
-
-    def index
-        @business = Business.find(params[:business_id])
-        @service = Service.find(params[:service_id])
-        @technicians = @business.find_technicians_by_service(@service)
-    end
-
-end
