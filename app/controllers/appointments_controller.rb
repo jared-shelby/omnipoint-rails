86e9dfc7db8ffda83ae6f3dc601d11c2e1679dd6
Bogdan Cuza <bogdancuza@protonmail.com>
2022-07-06 20:47:23 +0300

    Add other appointment fields

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -9,10 +9,13 @@ class AppointmentsController < ApplicationController
             @technicians = @business.technicians
             @service = Service.find(params[:service_id]) unless params[:service_id].nil?
             @technician = Technician.find(params[:technician_id]) unless (params[:technician_id].nil? || params[:service_id].nil?)
+            @offer = Offer.find_by(service: @service, technician: @technician) unless (@service.nil? || @technician.nil?)
+            @appointment = Appointment.new unless @offer.nil?
+            @form_url = @offer.nil? ? "/appointments/new" : "/appointments"
+            @form_method = @offer.nil? ? :get : :post
         end
     end
     def create
-        puts params
         @appointment = Appointment.new(
             client: Client.find_or_create_by(name: params[:client_name], number: params[:client_number], email: params[:client_email]),
             offer: Offer.find_by(service_id: params[:service_id], technician_id: params[:technician_id])
