8ad8b8f50cdde5a62e53a373bd879a4ab6e41693
Bogdan Cuza <bogdancuza@protonmail.com>
2022-07-06 21:05:37 +0300

    Add time view and calendar download

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -4,4 +4,8 @@ class Appointment < ApplicationRecord
   has_one :technician, through: :offer
   has_one :service, through: :offer
   has_one :business, through: :technician
+
+  def readable_time
+    self.time.strftime("%A, %B %d at %H:%M")
+  end
 end
