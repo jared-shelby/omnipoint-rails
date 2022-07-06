594e7377bef780ec83cd56632e0f901435b5c553
Jared Shelby <jared.shelby@yale.edu>
2022-07-06 09:28:38 -0400

    Seed file updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# create businesses
+Business.create(name: "Pretty Salon", location: "New York, NY", number: 1234567890)
+Business.create(name: "Eyebrow Central", location: "New Haven, CT", number: 9764443210)
+Business.create(name: "Derm Alert", location: "Chicago, IL", number: 8324506606)
+
+# create services
+Service.create(name: "Blowout", price: 30)
+Service.create(name: "Hair coloring", price: 60)
+Service.create(name: "Brow lift", price: 15)
+Service.create(name: "Brow coloring", price: 30)
+Service.create(name: "Beard treatment", price: 20)
+Service.create(name: "Facial", price: 25)
+Service.create(name: "Acne treatment", price: 50)
+
+# create technicians
+Technician.create(name: "Joan", business_id: 1)
+Technician.create(name: "Toby", business_id: 1)
+Technician.create(name: "Sam", business_id: 1)
+Technician.create(name: "Jessica", business_id: 2)
+Technician.create(name: "James", business_id: 2)
+Technician.create(name: "Fran", business_id: 2)
+Technician.create(name: "Pete", business_id: 3)
+Technician.create(name: "Wyatt", business_id: 3)
+
+# create offers
+Offer.create(technician_id: 1, service_id: 1)
+Offer.create(technician_id: 1, service_id: 2)
+Offer.create(technician_id: 2, service_id: 2)
+Offer.create(technician_id: 3, service_id: 1)
+Offer.create(technician_id: 4, service_id: 3)
+Offer.create(technician_id: 4, service_id: 4)
+Offer.create(technician_id: 5, service_id: 3)
+Offer.create(technician_id: 5, service_id: 4)
+Offer.create(technician_id: 6, service_id: 4)
+Offer.create(technician_id: 7, service_id: 6)
+Offer.create(technician_id: 7, service_id: 7)
+Offer.create(technician_id: 8, service_id: 6)
+Offer.create(technician_id: 8, service_id: 7)
