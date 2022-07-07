6c95fa623a72a472f02954fbd35fc7bfc0b7aba6
Jared Shelby <jared.shelby@yale.edu>
2022-07-07 13:15:32 -0400

    Validations added; not yet working

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,7 @@
 class Client < ApplicationRecord
     has_many :appointments
+
+    validates :name, presence: { message: "must contain your full name" }
+    validates :number, presence: { message: "must contain your phone number" }, numericality: { only_integer: true, message: "must be an integer" }, length: { is: 10, message: "must be in 'XXXXXXXXXX' format" }
+    validates :email, presence: { message: "must contain your email address" }
 end
