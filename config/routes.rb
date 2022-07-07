ad3359b34d3102e349cd554996d6f2c31db753b7
Jared Shelby <jared.shelby@yale.edu>
2022-07-07 09:38:29 -0400

    cleaner, more descriptive

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
 
-  # business routes
+  # home page
   get "/", to: "application#welcome"
+
+  # all businesses dropdown page -> individual business page w/ appointment form
   get "/businesses/welcome", to: "businesses#welcome"
   resources :businesses, only: [:index, :show]
 
-  #
+  # new appointment page houses the app flow; show confirmation page
   resources :appointments, only: [:new, :create, :show]
+
+  # calendar download link for appointment once confirmed
   get "/appointments/:id/calendar", to: "appointments#calendar"
-  #resources :clients
-  #resources :offers
-  #resources :services
-  #resources :technicians, only: [:index]
 
 end
