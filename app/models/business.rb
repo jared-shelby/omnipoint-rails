71fe2a7e549541cb6e41fe695697f2518e6cc216
Jared Shelby <jared.shelby@yale.edu>
2022-07-07 16:55:57 -0400

    edit/update ability for businesses initialized

--- app/models/business.rb
+++ app/models/business.rb
@@ -5,6 +5,9 @@ class Business < ApplicationRecord
     has_many :appointments, through: :offers
     has_many :clients, through: :appointments
 
+    validates :name, presence: { message: "must contain the full business name" }
+    validates :location, presence: { message: "must contain the location in 'City, State' format" }
+
     def find_technicians_by_service(service)
         self.technicians.filter { |technician| technician.services.include? service }
     end
