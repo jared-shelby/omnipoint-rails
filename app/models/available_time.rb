a5a5faac7f271a8aca1955b85a26c8952678d976
Jared Shelby <jared.shelby@yale.edu>
2022-07-07 23:40:34 -0400

    Seeding updates,datetime updates

--- app/models/available_time.rb
+++ app/models/available_time.rb
@@ -2,6 +2,6 @@ class AvailableTime < ApplicationRecord
     belongs_to :technician
 
     def readable
-        time.strftime("%A, %B %d at %H:%M")
+        time.strftime("%A, %B %d at %l:00%P")
     end
 end
